203c1339620d88867c12b31935c08e77349d9468
luoguofeng0401 <56261249+luoguofeng0401@users.noreply.github.com>
2024-05-08 07:30:28 +0800

    Add files via upload

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Guo-Feng Luo on 2024/5/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
